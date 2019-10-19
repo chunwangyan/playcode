72ee80fd43d708df12f883e8828c928ca9ea3ca9
yanchw <yanchw1990@163.com>
2019-10-19 18:33:11 +0800

    letcode->maxlensubstr增加滑动窗口算法

--- letCode/maxlensubstr.py
+++ letCode/maxlensubstr.py
@@ -1,4 +1,14 @@
+def alluniq(str):
+    sets = set()
+    for i in str:
+        sets.add(i)
+    if len(str) == len(sets):
+        return 1
+    else:
+        return 0
+
 class Solution:
+    # 暴力识别算法
     def lengthOfLongestSubstring(self, list):
         maxlen = 0
         for i in list:
@@ -17,20 +27,35 @@ class Solution:
                 results.append(str[i:i + x + 1])
         return results
 
-
-def alluniq(str):
-    sets = set()
-    for i in str:
-        sets.add(i)
-    if len(str) == len(sets):
-        return 1
-    else:
-        return 0
+    # 滑动窗口识别算法
+    def lengthOfLongestSubstringv2(self, str):
+        i = 0
+        j = 0
+        res = 0
+        substr = set()
+        while i < len(str) and j < len(str):
+            if substr.__contains__(str[j]) == False:
+                substr.add(str[j])
+                j = j + 1
+                if j - i > res:
+                    res = j - i
+            else:
+                substr.remove(str[i])
+                i = i + 1
+        return res
 
 
 if __name__ == '__main__':
-    obj = Solution()
-    str = "aaaaabcfrt"
-    substr = obj.cut(str)
-    print(obj.lengthOfLongestSubstring(substr))
-    # print(alluniq(str))
+    str = "abcabcabc"
+
+    # 第一种解法：暴力识别
+    # 先找出源字符串的所有子字符串，然后逐个判断字符是否重复，算出最长的不重复字串长度
+    obj01 = Solution()
+    substr_list = obj01.cut(str)
+    res01 = obj01.lengthOfLongestSubstring(substr_list)
+    print('暴力识别算法：', res01)
+    print('---------------------')
+    # 第二种解法：滑动窗口
+    obj02 = Solution()
+    res02 = obj02.lengthOfLongestSubstringv2(str)
+    print('滑动窗口算法：', res02)
--- test/listToStr.py
+++ test/listToStr.py
@@ -1,15 +1,21 @@
-import re
-
+# import re
 #
+# #
+#
+# # str = ','.join(str(item) for inneritem in list for item in inneritem)
+#
+# # keywords = ('[', ']')
+# # restr = re.sub('|'.join(keywords), '', '[[10, 20], [39, 182], [61, 90]]')
+#
+#
+# s = '[]'
+# str = '[[10, 20], [39, 182], [61, 90]]'
+# a = re.findall('[\u4e00-\u9fa5a-zA-Z0-9]+', str, re.S)
+# restr = ",".join(a)
+# print(restr)
 
-# str = ','.join(str(item) for inneritem in list for item in inneritem)
-
-# keywords = ('[', ']')
-# restr = re.sub('|'.join(keywords), '', '[[10, 20], [39, 182], [61, 90]]')
-
-
-s = '[]'
-str = '[[10, 20], [39, 182], [61, 90]]'
-a = re.findall('[\u4e00-\u9fa5a-zA-Z0-9]+', str, re.S)
-restr = ",".join(a)
-print(restr)
+sets = set()
+sets.add(1)
+sets.add(2)
+if sets.__contains__(1) == True:
+    print('yes')
